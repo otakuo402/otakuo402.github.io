1b13e398eb508e8fbd94fd032ef3dd5fe136c47d
otakuo402 <jhterng@umich.edu>
2022-12-04 22:54:04 -0500

    Update React Router for gh-pages support

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { Homepage } from './Homepage';
 import { Blog } from './Blog';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Routes>
-          <Route path="/" element={ <Navigate to="/home" /> } />
-          <Route path="blog" element={ <Blog/> } />
-          <Route path="/home" element={ <Homepage/> } />
-        </Routes>
-      </header>
-      
-    </div>
+    <BrowserRouter history={window.history} basename={process.env.PUBLIC_URL}>
+      <div className="App">
+        <header className="App-header">
+          <Routes>
+            <Route path="/" element={ <Navigate to="/home" /> } />
+            <Route path="blog" element={ <Blog/> } />
+            <Route path="/home" element={ <Homepage/> } />
+          </Routes>
+        </header>
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
