a95a2ee9284c3622c7ab2ff6cdca8074ae34ae09
otakuo402 <jhterng@umich.edu>
2022-12-08 12:42:23 -0500

    Update fetch to only occur once and not put unnecessary bog on page

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,21 @@ import { Homepage } from './Homepage';
 import { Blog } from './Blog';
 import { Navigation } from './Navigation';
 import BlogPages from './BlogPages';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [markdowns, setMarkdowns] = useState([]);
 
-  fetch('/allFileNames.txt')
-  .then((r) => r.text())
-  .then(text  => {
-    var array = text.split(/\r?\n/);
-    array.pop();
-    setMarkdowns(array)
-  })
-  .catch(err => console.log(err));
+  useEffect(() => {
+    fetch('/allFileNames.txt')
+      .then((r) => r.text())
+      .then(text  => {
+        var array = text.split(/\r?\n/);
+        array.pop();
+        setMarkdowns(array)
+      })
+      .catch(err => console.log(err));
+  },[]);
   
   const routeMarkdownFiles = markdowns.map((file, index) =>
     <Route key={index} path={"blog/" + file} element={ <BlogPages post={file} /> } />
